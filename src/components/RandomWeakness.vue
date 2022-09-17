569761b505917b6f49d6d164446e7e810001ddd6
Eric Endsley <eric.endsley4@gmail.com>
2022-09-17 16:03:47 -0700

    refactor: dynamically generate checkboxes from data

--- src/components/RandomWeakness.vue
+++ src/components/RandomWeakness.vue
@@ -8,34 +8,80 @@ export default {
       ownedSets: [],
       ownedCards: [],
       setCodes: {
-        core: [
-          '01096',
-          '01096',
-          '01097',
-          '01097',
-          '01098',
-          '01099',
-          '01100',
-          '01101',
-          '01102',
-          '01103',
-        ],
-        dwl: ['02037', '02037', '02038', '02038', '02039', '02039'],
-        ptc: ['03040', '03040', '03041', '03042'],
-        tfa: ['04038', '04040'],
-        tcu: ['05041', '05041', '05042', '05042'],
-        tde: ['06035', '06036', '06037', '06038'],
-        tic: ['07038', '07038', '07039', '07039', '07040'],
-        eote: ['08130', '08131', '08132', '08133'],
-        rtdwl: ['51011', '51011'],
-        rtptc: ['52011', '52012', '52013'],
-        rttfa: ['53012', '53013'],
-        rttcu: ['54014', '54015', '54015'],
-        nate: ['60104'],
-        harvey: ['60204'],
-        wini: ['60304'],
-        jackie: ['60404'],
-        stella: ['60504'],
+        core: {
+          codes: [
+            '01096',
+            '01096',
+            '01097',
+            '01097',
+            '01098',
+            '01099',
+            '01100',
+            '01101',
+            '01102',
+            '01103',
+          ],
+          friendlyName: 'Core Set',
+        },
+        dwl: {
+          codes: [
+            '02037',
+            '02037',
+            '02038',
+            '02038',
+            '02039',
+            '02039',
+          ],
+          friendlyName: 'Dunwich Legacy',
+        },
+        ptc: {
+          codes: ['03040', '03040', '03041', '03042'],
+          friendlyName: 'Path to Carcosa',
+        },
+        tfa: {
+          codes: ['04038', '04040'],
+          friendlyName: 'Forgotten Age',
+        },
+        tcu: {
+          codes: ['05041', '05041', '05042', '05042'],
+          friendlyName: 'Circle Undone',
+        },
+        tde: {
+          codes: ['06035', '06036', '06037', '06038'],
+          friendlyName: 'Dream-Eaters',
+        },
+        tic: {
+          codes: ['07038', '07038', '07039', '07039', '07040'],
+          friendlyName: 'Innsmouth Conspiracy',
+        },
+        eote: {
+          codes: ['08130', '08131', '08132', '08133'],
+          friendlyName: 'Edge of the Earth',
+        },
+        rtdwl: {
+          codes: ['51011', '51011'],
+          friendlyName: 'Return to DWL',
+        },
+        rtptc: {
+          codes: ['52011', '52012', '52013'],
+          friendlyName: 'Retrun to PtC',
+        },
+        rttfa: {
+          codes: ['53012', '53013'],
+          friendlyName: 'Return to TFA',
+        },
+        rttcu: {
+          codes: ['54014', '54015', '54015'],
+          friendlyName: 'Return to TCU',
+        },
+        nate: { codes: ['60104'], friendlyName: 'Nathaniel Cho' },
+        harvey: { codes: ['60204'], friendlyName: 'Harvey Walters' },
+        wini: {
+          codes: ['60304'],
+          friendlyName: 'Winifred Habbamock',
+        },
+        jackie: { codes: ['60404'], friendlyName: 'Jacqueline Fine' },
+        stella: { codes: ['60504'], friendlyName: 'Stella Clarke' },
       },
       weaknessImageUrl: null,
     };
@@ -61,7 +107,7 @@ export default {
       this.ownedCards = [];
 
       this.ownedSets.forEach((set) => {
-        this.ownedCards.push(...this.setCodes[set]);
+        this.ownedCards.push(...this.setCodes[set].codes);
       });
     },
   },
@@ -79,23 +125,13 @@ export default {
       Generate Random Basic Weakness
     </el-button>
     <el-checkbox-group @change="addSetCards" v-model="ownedSets">
-      <el-checkbox label="core">Core Set</el-checkbox>
-      <el-checkbox label="dwl">Dunwich Legacy</el-checkbox>
-      <el-checkbox label="ptc">Path to Carcosa</el-checkbox>
-      <el-checkbox label="tfa">Forgotten Age</el-checkbox>
-      <el-checkbox label="tcu">Circle Undone</el-checkbox>
-      <el-checkbox label="tde">Dream-Eaters</el-checkbox>
-      <el-checkbox label="tic">Innsmouth Conspiracy</el-checkbox>
-      <el-checkbox label="eote">The Edge of the Earth</el-checkbox>
-      <el-checkbox label="rtdwl">Return to DL</el-checkbox>
-      <el-checkbox label="rtptc">Return to PtC</el-checkbox>
-      <el-checkbox label="rttfa">Return to TFA</el-checkbox>
-      <el-checkbox label="rttcu">Return to CU</el-checkbox>
-      <el-checkbox label="nate">Nathaniel Cho</el-checkbox>
-      <el-checkbox label="harvey">Harvey Walters</el-checkbox>
-      <el-checkbox label="wini">Winifred Habbamock</el-checkbox>
-      <el-checkbox label="jackie">Jacqueline Fine</el-checkbox>
-      <el-checkbox label="stella">Stella Clark</el-checkbox>
+      <el-checkbox
+        v-for="(set, key) in setCodes"
+        :label="key"
+        :key="key"
+      >
+        {{ set.friendlyName }}
+      </el-checkbox>
     </el-checkbox-group>
     <img class="weakness-image" :src="weaknessImageUrl" />
   </div>
